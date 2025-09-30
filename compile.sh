ccfc1e13f82c14165bd30555a13a556ca9a239d1
3manuel0 <saiddimension@gmail.com>
2025-09-30 21:51:15 +0100

    first commit

--- compile.sh
+++ compile.sh
@@ -1,3 +1,6 @@
 nasm -f elf64 -o test.o write_to_file.asm &&\
-ld -o test test.o -dynamic-linker /lib64/ld-linux-x86-64.so
+
+ld -o test test.o -dynamic-linker /lib64/ld-linux-x86-64.so &&\
+
+./test
 
