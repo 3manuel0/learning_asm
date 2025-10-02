d7b31d6fad33755aae12fcc22306debef37c4f1e
3manuel0 <saiddimension@gmail.com>
2025-10-03 00:40:43 +0100

    putnbr unsigned

--- putnbr.asm
+++ putnbr.asm
@@ -1,21 +1,18 @@
 BITS 64
 ; 18446744073709551615
 section .data
-    numb: dq -99222
-    errmsg: db "number exceded the int limit", 10 
+    numb: dq 6744073709
+    errmsg: db "number exceeded the int limit", 0xa 
     msglen equ $ - errmsg
 section .bss
-    val: resb 1
-    x: resq 1
-    numlen: resq 1
     buffer resb 11 
 section .text
 global _start
 _start: 
-    mov rax, [numb]
+    mov rax, [numb] ;
     len:
-        inc byte [x]
-        inc byte [numlen]
+        inc r8
+        inc r9
         xor rdx, rdx
         mov rcx, 10
         div rcx
@@ -23,20 +20,19 @@ _start:
         jne len
     putnbr:
         ; mov rax, 255
-        dec qword [x]
-        cmp qword [x], 10
-        jg error ; jump if [x] > 10
-        xor rdx, rdx
-        mov rax, [numb]
+        dec r8
+        cmp r8, 10
+        jg error ; jump if r8 > 10
+        xor rdx, rdx ; rdx = 0 
+        mov rax, [numb] ; rax = *p
         mov rcx, 10
         div rcx
         mov [numb], rax
-        mov [val], dl ; rdx modulo 8 bits
-        add byte [val], 48 ; 5 + 48-> 5 + '0'-> '5'
-        mov rcx, [x]
-        mov al, [val] 
-        mov byte [buffer + rcx], al ; buffer[x] = al
-        cmp qword [numb], 0
+        mov r11b, dl ; rdx modulo 8 bits
+        add byte r11b, 48 ; 5 + 48-> 5 + '0'-> '5'
+        mov rcx, r8
+        mov byte [buffer + rcx], byte r11b ; buffer[rcx] = r11b
+        cmp qword [numb], 0 ; 64 bit
         je exit
         jmp putnbr
     exit:
@@ -44,12 +40,12 @@ _start:
         mov rax, 1; write
         mov rdi, 1; fd 
         lea rsi, [buffer]; buff
-        mov rcx, [numlen];len
-        mov byte [buffer + rcx], byte 10; buffer[6] = '\n'
-        inc qword [numlen]
-        mov rdx, [numlen]; len
+        mov rcx, r9; len
+        mov byte [buffer + rcx], byte 10; buffer[rcx] = '\n'
+        inc qword r9
+        mov rdx, r9; len
         syscall
-        mov rax, 0x3c      ; exit(.)
+        mov rax, 0x3c  ; exit(.)
         mov rdi, 0     ; exit status 0
         syscall        
     error:
@@ -58,6 +54,6 @@ _start:
         lea rsi, errmsg ; buf
         mov rdx, msglen ; len
         syscall
-        mov rax, 0x3c      ; exit(.)
-        mov rdi, 0      ; exit status 0
+        mov rax, 0x3c ; exit(.)
+        mov rdi, 0   ; exit status 0
         syscall    
